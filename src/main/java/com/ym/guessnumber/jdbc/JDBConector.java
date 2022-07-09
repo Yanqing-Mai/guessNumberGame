f1034bee49ca95cbe292c4de52d3d9c138dbb163
Amy <amy.mai97@yahoo.com>
2022-07-08 20:16:12 -0500

    refactor database connection

--- src/main/java/com/ym/guessnumber/jdbc/JDBConector.java
+++ src/main/java/com/ym/guessnumber/jdbc/JDBConector.java
@@ -7,14 +7,5 @@ import java.sql.SQLException;
 
 public class JDBConector {
 
-    private static DataSource getDatasource() throws SQLException{
-        MysqlDataSource ds = new MysqlDataSource();
-        ds.setServerName("localhost");
-        ds.setDatabaseName("gamesDB");
-        ds.setUser("root");
-        ds.setPassword("010297");
-        ds.setServerTimezone("America/Central");
-        ds.setAllowPublicKeyRetrieval(true);
-        return ds;
-    }
+
 }
